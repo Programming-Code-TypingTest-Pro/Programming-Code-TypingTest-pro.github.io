288655894688c5fc0bc286d47416c00a345d1a5a
Programming-Code-TypingTest-Pro <princegarad742@gmail.com>
2026-08-06 16:09:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11514824
+    "zoneId": 11515131
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
