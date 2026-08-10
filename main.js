16b654c911e80463968b9d7d789ce0f28b197e48
Programming-Code-TypingTest-Pro <princegarad742@gmail.com>
2026-08-10 17:19:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ if (canvas) {
 // ==========================================
 // GLOBAL VARIABLES & UTILS
 // ==========================================
-const languages = ["English","Python","Java","JavaScript","TypeScript","C++","C#","R","Kotlin","HTML","CSS","Swift","Node.js","Dart","Rust","PHP","SQL","Bash","PowerShell","Go","Ruby","Scal","Matlab","Julia","Lua","Perl","VB.NET","F#","Haskell","Elixir","Erlang","COBOL","Fortran","Groovy","Clojure","OCaml","Prolog","Ada","Nim","Zig","Solidity","Assembly","Objective-C"];
+const languages = ["English","Python","Java","JavaS_t","TypeS_t","C++","C#","R","Kotlin","HTML","CSS","Swift","Node.js","Dart","Rust","PHP","SQL","Bash","PowerShell","Go","Ruby","Scal","Matlab","Julia","Lua","Perl","VB.NET","F#","Haskell","Elixir","Erlang","COBOL","Fortran","Groovy","Clojure","OCaml","Prolog","Ada","Nim","Zig","Solidity","Assembly","Objective-C"];
 
 let selectedProjects = [];
 let selectedCode = "";
@@ -138,7 +138,7 @@ function initPage1() {
         languages.forEach(lang => {
           let btn = document.createElement("button");
           if(lang === "Python" || lang === "Java" || lang === "C++" || lang === "R" || lang === "JavaScript" || lang === "TypeScript" || lang === "Kotlin" || lang === "C#" || lang === "English"){  
-            btn.innerText = "⭐ " + lang;
+            btn.innerText = "⭐" + lang;
           } else {
             btn.innerText = lang;
           }
